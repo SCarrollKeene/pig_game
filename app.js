70fe0b27358a728a4de79da5c7b36208d983c9ba
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-05-17 10:18:19 -0600

    updated winning score to 100 in the if/else statement on line 58 of the app.js file

--- app.js
+++ app.js
@@ -41,27 +41,52 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next Player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        // set round score back to zero
-        roundScore = 0;
-
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // Display who active player is
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        //document.querySelector('.player-0-panel').classList.remove('active');
-        //document.querySelector('.player-1-panel').classList.add('active');
-
-        // When player rolls a 1, hide dice again
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
 
+    // 1. Add CURRENT score to players' GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        //document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+
+    // Next Player
+    nextPlayer();
+});
+
+function nextPlayer() {
+     // Next Player
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+     // set round score back to zero
+     roundScore = 0;
+
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+
+     // Display who active player is
+     document.querySelector('.player-0-panel').classList.toggle('active');
+     document.querySelector('.player-1-panel').classList.toggle('active');
+
+     //document.querySelector('.player-0-panel').classList.remove('active');
+     //document.querySelector('.player-1-panel').classList.add('active');
+
+     // When player rolls a 1, hide dice again
+     document.querySelector('.dice').style.display = 'none';
+}
 
 
 
