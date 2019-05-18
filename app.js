81070e949b54167b657ce6283d52950ed5d4e2cd
Stephen Carroll-Keene <stephen.carrollkeene@gmail.com>
2019-05-17 20:11:11 -0600

    updated game so once a player wins the game it will not indicate who the next player in the round is by removing the active css class on line 62

--- app.js
+++ app.js
@@ -59,13 +59,11 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        //document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     } else {
+        // Next Player
         nextPlayer();
     }
-
-    // Next Player
-    nextPlayer();
 });
 
 function nextPlayer() {
